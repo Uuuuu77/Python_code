37b7366d2ea34b39bbee3e5222348ed1a5829e61
John <125473490+Uuuuu77@users.noreply.github.com>
2023-09-15 17:26:16 +0300

    Talk about loops, if and functon statement

---
+++ Hello.py
@@ -0,0 +1,21 @@
+name = "john"
+name
+print(name)
+for x in name:
+    print(x)
+
+y = 0
+while y < len(name):
+    print(y)
+    y += 1
+
+name_1 = "Steve"
+Fullname = name_1 + ' ' + name
+print(Fullname)
+
+
+def Student(x, y):
+    print(x + y)
+
+Name_of_student = Student("Steve", " jobs")
+print(Name_of_student)
