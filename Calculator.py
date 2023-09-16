3b511c54fa98a57f5a4fdf485c8484cdb5266740
John <125473490+Uuuuu77@users.noreply.github.com>
2023-09-16 13:23:25 +0300

    My calculator

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+# Building Calculator using python
+num_1, operator, num_2 = input("Enter number: ").split( )
+
+num_1 = int(num_1) # Converts to integer
+num_2 = int(num_2) # converts to integer
+
+if operator == "+":
+    print("{} + {} = {}".format(num_1, num_2, num_1 + num_2))
+elif operator == "-":
+    print("{} - {} = {}".format(num_1, num_2, num_1 - num_2))
+elif operator == "*":
+    print("{} * {} = {}".format(num_1, num_2, num_1 * num_2))
+elif operator == "/":
+    print("{} / {} = {}".format(num_1, num_2, num_1 / num_2))
+else:
+    print("Enter the correct operator{+, -, *, /}")
+    
