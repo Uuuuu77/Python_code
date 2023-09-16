8f9d4c770e4e1ffb6d3c3967448a6b38afe047e9
John <125473490+Uuuuu77@users.noreply.github.com>
2023-09-16 13:25:07 +0300

    My calculator

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,5 @@
 # Building Calculator using python
-num_1, operator, num_2 = input("Enter number: ").split( )
+num_1, operator, num_2 = input("Enter number: ").split()
 
 num_1 = int(num_1) # Converts to integer
 num_2 = int(num_2) # converts to integer
