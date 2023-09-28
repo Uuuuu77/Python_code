651b111299c7d8ab1a8e55cf22dcfa96718369af
John <125473490+Uuuuu77@users.noreply.github.com>
2023-09-28 11:08:48 +0300

    More in class and inheritance in python

--- OOP.py
+++ OOP.py
@@ -51,18 +51,92 @@ class Square:
     def get_area(self):
         return int(self.__height) * int(self.__width)
 
-def main():
-    _Square = Square()
+#def main():
+#    _Square = Square()
+#
+#   height = int(input("Enter the height: "))
+#    width = int(input("Enter the width: "))
+#
+#    _Square.height = height
+#    _Square.width = width
+#
+#    print("Height = ", _Square.height)
+#    print("Width = ", _Square.width)
+#    print("The Area = ", _Square.get_area())
 
-    height = int(input("Enter the height: "))
-    width = int(input("Enter the width: "))
-
-    _Square.height = height
-    _Square.width = width
-
-    print("Height = ", _Square.height)
-    print("Width = ", _Square.width)
-    print("The Area = ", _Square.get_area())
-
-main()
+#main()
 # square needs to be debug
+
+""" Inheritance magic method in python """
+
+class Animal:
+    def __init__(self, birth_type="Unknown", appearance="Unknown", blooded="Unknown"):
+        self.birth_type = birth_type
+        self.appearance = appearance
+        self.blooded = blooded
+    
+    @property
+    def birth_type(self):
+        return self.__birth_type
+    
+    @birth_type.setter
+    def birth_type(self, birth_type):
+        self.__birth_type = birth_type
+    
+    @property
+    def appearance(self):
+        return self.__appearance
+    
+    @appearance.setter
+    def appearance(self, appearance):
+        self.__appearance = appearance
+    
+    @property
+    def blooded(self):
+        return self.__blooded
+    
+    @blooded.setter
+    def blooded(self, blooded):
+        self.__blooded = blooded
+    
+    def __str__(self):
+        return "A {} is {} it is {} it is {}".format(type(self).__name__, self.birth_type, self.appearance, self.blooded)
+    
+class Mammals:
+    def __init__(self, birth_type="born alive", appearance="hair or fur", blooded="warm-blooded", nurse_young=True):
+        Animal.__init__(self, birth_type, appearance, blooded)
+        self.nurse_young = nurse_young
+        
+    @property
+    def nurse_young(self):
+        return self.__nurse_young
+    
+    @nurse_young.setter
+    def nurse_young(self, nurse_young):
+        self.__nurse_young = nurse_young
+        
+    def __str__(self):
+        return super().__str__() + " and it is {} who the nurse their young".format(self.nurse_young)
+
+class Reptile(Animal):
+    def __init__(self, birth_type="born in an egg", appearance="dry scales", blooded="cold-blooded"):
+        Animal.__init__(self, birth_type, appearance, blooded)
+    
+    def sumAll(self, *args):
+        sum = 0
+
+        for x in args:
+            sum += x
+        return sum
+
+def main():
+    Animal_1 = Animal("born alive")
+    print(Animal_1.birth_type)
+    print(Animal_1)
+    print()
+
+    Mammals_1 = Mammals()
+    print(Mammals_1.birth_type)
+    print(Mammals_1.appearance)
+    print(Mammals_1.blooded)
+main()
